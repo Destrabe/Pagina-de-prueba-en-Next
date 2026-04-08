8e4dd9246e618b92bc91ad48836c56885a81322d
Destrabe20 <marco96392@gmail.com>
2026-04-08 05:20:02 -0500

    refactor: use environment variables for Firebase configuration

--- firebase/client.js
+++ firebase/client.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyARnUlppeS7M7QXP5E7h9TkyEcfmSGd3jc",
-  authDomain: "prueba-21911.firebaseapp.com",
-  projectId: "prueba-21911",
-  storageBucket: "prueba-21911.firebasestorage.app",
-  messagingSenderId: "264352816079",
-  appId: "1:264352816079:web:ce12c388b85ce4939f90ba",
-  measurementId: "G-3L7PDCDHJM",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
